b6aa6fc7b35170d7f419668fa67008cdc59e8a08
Nikita-Mohite <mohitenikita234@gmail.com>
2025-04-21 03:12:27 +0530

    this is java class

---
+++ src/pack/Tests.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "December";
+		s = s.concat("is a holiday season");
+		System.out.println(s);
+	}
+
+}
